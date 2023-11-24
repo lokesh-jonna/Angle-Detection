a01bc326b993e175fc3e5140724eb52bbcac7dcb
lokesh-jonna <jonna.lokesh1@gmail.com>
2023-11-24 21:33:06 +0530

    python_code.py

--- python_code.py
+++ python_code.py
@@ -1,110 +1,94 @@
 import cv2
 import numpy as np
 
-def edge_detection(img, blur_ksize=3, threshold1=50, threshold2=50):
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    img_gaussian = cv2.GaussianBlur(gray, (blur_ksize, blur_ksize), 0)
-    img_canny = cv2.Canny(img_gaussian, threshold1, threshold2)
+# Load YOLO model
+net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
+classes = []
+with open("coco.names", "r") as f:
+    classes = [line.strip() for line in f.readlines()]
 
-    return img_canny
-
-
-def draw_lines(img, lines, color=[255, 0, 0], thickness=3):
-    for line in lines:
-        for x1, y1, x2, y2 in line:
-            cv2.line(img, (x1, y1), (x2, y2), color, thickness)
-
-    if lines is not None:
-        for line in lines:
-            for x1, y1, x2, y2 in line:
-                if x2 - x1 == 0:
-                    cv2.line(img, (x1, 0), (x1, img.shape[0]), color, thickness)
-                else:
-                    # calculate slope and y-intercept
-                    m = (y2 - y1) / (x2 - x1)
-                    b = y1 - m * x1
-
-                    # calculate endpoints of line at top and bottom of image
-                    y_top = 0
-                    x_top = (y_top - b) / m
-                    y_bottom = img.shape[0]
-                    x_bottom = (y_bottom - b) / m
-
-                    # draw line
-                    cv2.line(img, (int(x_top), y_top), (int(x_bottom), y_bottom), color, thickness)
-
-
-def get_angle(image):
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    edges = cv2.Canny(gray, 50, 150, apertureSize=3)
-    lines = cv2.HoughLines(edges, 1, np.pi/180, 200)
-
-    # Remove the line that prints the detected lines
-
-    # Rest of the code...
-
-    # extract the angle
-    angle = 0
-    if lines is not None and len(lines) > 0:
-        for line in lines:
-            for rho, theta in line:
-                a = np.cos(theta)
-                b = np.sin(theta)
-                x0 = a * rho
-                y0 = b * rho
-                x1 = int(x0 + 1000 * (-b))
-                y1 = int(y0 + 1000 * (a))
-                x2 = int(x0 - 1000 * (-b))
-                y2 = int(y0 - 1000 * (a))
-                cv2.line(image, (x1, y1), (x2, y2), (0, 0, 255), 2)
-                angle = np.arctan2(y2 - y1, x2 - x1)
-                angle = angle * 180 / np.pi
-                angle = abs(angle)
-                if angle > 180:
-                    angle = 360 - angle
-        if angle != 0:
-            return angle
-    return None
-
-
+# Open camera capture
 cap = cv2.VideoCapture(0)
 
+# Initialize variables to store detected objects
+detected_objects = []
+
 while True:
+    # Read frame from the camera
     ret, frame = cap.read()
 
-    if not ret:
+    # Preprocess input
+    blob = cv2.dnn.blobFromImage(frame, 1/255, (416, 416), swapRB=True, crop=False)
+    net.setInput(blob)
+
+    # Perform object detection
+    output_layers_names = net.getUnconnectedOutLayersNames()
+    outputs = net.forward(output_layers_names)
+
+    # Postprocess the results
+    boxes = []
+    confidences = []
+    class_ids = []
+
+    for output in outputs:
+        for detection in output:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+
+            if confidence > 0.5:  # Adjust confidence threshold as needed
+                center_x = int(detection[0] * frame.shape[1])
+                center_y = int(detection[1] * frame.shape[0])
+                w = int(detection[2] * frame.shape[1])
+                h = int(detection[3] * frame.shape[0])
+                x = int(center_x - w/2)
+                y = int(center_y - h/2)
+
+                boxes.append([x, y, w, h])
+                confidences.append(float(confidence))
+                class_ids.append(class_id)
+
+    # Apply non-maximum suppression to remove redundant boxes
+    indices = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+
+    # Draw bounding boxes and labels
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    colors = np.random.uniform(0, 255, size=(len(classes), 3))
+
+    if len(indices) > 0:
+        detected_objects = []  # Clear the list of detected objects for the current frame
+        for i in indices.flatten():
+            x, y, w, h = boxes[i]
+            label = classes[class_ids[i]]
+            confidence = confidences[i]
+            color = colors[class_ids[i]]
+
+            cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+            cv2.putText(frame, f"{label}: {confidence:.2f}", (x, y - 5), font, 0.5, color, 2)
+
+            detected_objects.append((x, y, x + w, y + h, label))
+
+    # Calculate angle between newly added objects when there are at least two
+    if len(detected_objects) >= 2:
+        object1, object2 = detected_objects[:2]
+        x1, y1, x2, y2, label1 = object1
+        x3, y3, x4, y4, label2 = object2
+
+        # Calculate the angle between the two objects
+        angle_between_objects = np.degrees(np.arctan2(y4 - y3, x4 - x3) - np.arctan2(y2 - y1, x2 - x1))
+
+        # Display the calculated angle on the frame
+        cv2.putText(frame, f"Angle: {angle_between_objects:.2f}", (10, 30), font, 1, (0, 255, 0), 2, cv2.LINE_AA)
+
+        print("Angle between objects: {:.2f}".format(angle_between_objects))
+
+    # Display the frame
+    cv2.imshow("Object Detection", frame)
+
+    # Break the loop on 'q' key press
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-    cv2.imshow('frame', frame)
-
-    # wait for 'q' key to start detecting object or exit
-    key = cv2.waitKey(1) & 0xFF
-    if key == ord('q'):
-        break
-
-object_detected = False
-
-while not object_detected:
-    ret, frame = cap.read()
-
-    if not ret:
-        break
-
-    angle = get_angle(frame)
-    if angle is not None:
-        print("Angle: {:.2f}".format(angle))
-        object_detected = True
-
-    cv2.imshow('frame', frame)
-    key = cv2.waitKey(1) & 0xFF
-    if key == ord('q'):
-        break
-    elif key == ord('s'):
-        # save the frame as an image
-        filename = "object_image.jpg"
-        cv2.imwrite(filename, frame)
-        print(f"Object image saved as {filename}")
-        object_detected = True
-
+# Release the capture and close windows
 cap.release()
 cv2.destroyAllWindows()
